e3ebbd50cf03161b63648b62d2bb4c7a7f0e13a7
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-11-05 16:34:52 +0900

    initial setting

---
+++ src/main/java/com/example/techer_partners_api_practice/TecherPartnersApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.techer_partners_api_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecherPartnersApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecherPartnersApiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/techer_partners_api_practice/TecherPartnersApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techer_partners_api_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecherPartnersApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
